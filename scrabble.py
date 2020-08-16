5bef72dcd0efbeba26de71e7884ad1bfaae859b2
cchauvin-raja <52909281+cchauvin-raja@users.noreply.github.com>
2020-08-16 12:41:07 +0200

    Add files via upload

--- scrabble.py
+++ scrabble.py
@@ -1,2 +1,69 @@
 # scrabble.py
 # chauvin
+# def enleve_accents(mot):
+import unidecode
+import pickle
+
+fname = '/Users/cchauvin/Documents/RH-Perso/Scrabble/visu/' + 'liste.de.mots.francais.frgut.txt'
+with open(fname) as f:
+    mots_bruts = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+mots_bruts = [x.strip() for x in mots_bruts]
+mots_bruts = [unidecode.unidecode(x) for x in mots_bruts]
+set_de_mots = set(mots_bruts)
+
+dict_tableau_vers_mot ={}
+for mot in set_de_mots:
+    liste_de_lettres = [x for x in mot]
+    liste_de_lettres.sort()
+    valeur_du_dict = dict_tableau_vers_mot.get(''.join(liste_de_lettres))
+    if valeur_du_dict is None:
+        dict_tableau_vers_mot[''.join(liste_de_lettres)] = mot
+    elif isinstance(valeur_du_dict,str):
+        tableau_temp = []
+        tableau_temp.append(valeur_du_dict)
+        tableau_temp.append(mot)
+        dict_tableau_vers_mot[''.join(liste_de_lettres)] = tableau_temp
+    else:
+        dict_tableau_vers_mot[''.join(liste_de_lettres)] = valeur_du_dict.append(mot)
+    # si plusieurs mots pour un litste fair un tablea
+    pass
+mon_fichier_dico = open('/Users/cchauvin/Documents/RH-Perso/Scrabble/visu/Dico.bin','wb')
+pickle.dump(dict_tableau_vers_mot,mon_fichier_dico)
+mon_fichier_dico.close
+
+# liaison entre un mot et un tableau de lettres trié
+# quand je prend un tirage je le transforme en tableau trié et je voies les mots possibles
+
+# télécharger la liste des mots http://www.pallier.org/liste-de-mots-francais.html
+#  pip install Unidecode
+# transformer tous les mots avec b= unidecode.unidecode(a)
+# en faire un set
+# retirer les mots composés
+#############################
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def myfunc(self):
+#     print("Hello my name is " + self.name)
+
+# p1 = Person("John", 36)
+#############################
+
+# class reglette:
+#     def __init__(self):
+#         lettre
+    
+#     def tire_lettre(self,sac_de_lettre):
+
+#     def pose_mot(self,grille):
+#         # pour tous les mots de la grille, l'ajout de lettres fait il un mot 
+
+# class grille_scrabble:
+#     def __init__(self):
+
+#     def ajoute(mot):
+
+#     def valorise_mot(mot,coord_x,coord_y)
--- scrabble2.py
+++ scrabble2.py
@@ -4,8 +4,50 @@ mon_fichier_dico = open('/Users/cchauvin/Documents/RH-Perso/Scrabble/visu/Dico.b
 dict_tableau_vers_mot = pickle.load(mon_fichier_dico)
 mon_fichier_dico.close()
 
-print('Veuillez entrer les lettres : ')
-lettres = input('Veuillez entrer les lettres : ')
+# print('Veuillez entrer les lettres : ')
+# lettres = input('Veuillez entrer les lettres : ')
+lettres = 'oolpag'
+meslettres = sorted(lettres)
+
+def combinaison_lettres(tirage, longueur):
+    # permet de faire toutes les combinaisaons de k lettre à partir d'un tableau
+    # à transformer avec chaines
+    p = []
+    i, imax = 0, 2**len(tirage)-1
+    while i<=imax:
+        s = []
+        j, jmax = 0, len(tirage)-1
+        while j<=jmax:
+            if (i>>j)&1==1:
+                s.append(tirage[j])
+            j += 1
+        if len(s)==longueur:
+            p.append(''.join(sorted(s)))
+        i += 1 
+    return p
+
+def trouve_les_combinaisons(lettres):
+    tableau_combi=[]
+    for z  in range(len(lettres)):
+        for combinaison in combinaison_lettres(lettres,z):
+            if len(combinaison) >= 2 :
+                tableau_combi.append(combinaison)
+    tableau_combi.append(lettres)
+    return tableau_combi
+
+
+toutes_les_combinaisons = trouve_les_combinaisons(lettres)
+
+toutes_les_solutions=[]
+for combinaison in toutes_les_combinaisons:
+    solutions = dict_tableau_vers_mot.get(combinaison) 
+    if solutions is not None:
+        if isinstance(solutions,list):
+            for solution in solutions:
+                toutes_les_solutions.append(solution)
+        else:
+            toutes_les_solutions.append(solutions)
+
 
 tutu = dict_tableau_vers_mot.get(lettres)
 if isinstance (tutu,list):
@@ -14,3 +56,4 @@ if isinstance (tutu,list):
 else:
     print('******* '+ tutu+' ********')
 
+test
